bc72ba79a4f49e2ba38a13ce76895b0cca5a3a89
Constantin Suiu <constantinsuiu@Constantins-iMac.local>
2018-10-02 12:56:04 +0200

    Complete challenge 03

--- 03/tags.py
+++ 03/tags.py
@@ -8,27 +8,41 @@ TOP_NUMBER = 10
 RSS_FEED = 'rss.xml'
 SIMILAR = 0.87
 TAG_HTML = re.compile(r'<category>([^<]+)</category>')
+REPLACE_CHARS = str.maketrans('-', ' ')
 
 
 def get_tags():
     """Find all tags (TAG_HTML) in RSS_FEED.
     Replace dash with whitespace.
     Hint: use TAG_HTML.findall"""
-    pass
+    """with open(RSS_FEED, mode="r") as file:
+        data = file.read()
+        return TAG_HTML.findall(data)"""
+
+    with open(RSS_FEED) as f:
+        tags = TAG_HTML.findall(f.read().lower())
+    return [tag.translate(REPLACE_CHARS) for tag in tags]
 
 
 def get_top_tags(tags):
     """Get the TOP_NUMBER of most common tags
     Hint: use most_common method of Counter (already imported)"""
-    pass
-
+    counter = Counter(tags)
+    return counter.most_common(TOP_NUMBER)
 
 def get_similarities(tags):
     """Find set of tags pairs with similarity ratio of > SIMILAR
     Hint 1: compare each tag, use for in for, or product from itertools (already imported)
     Hint 2: use SequenceMatcher (imported) to calculate the similarity ratio
     Bonus: for performance gain compare the first char of each tag in pair and continue if not the same"""
-    pass
+    for pair in product(tags, tags):
+        if pair[0][0] != pair[1][0]:
+            continue
+        pair = tuple(sorted(pair))
+        similarity = SequenceMatcher(None, pair[0], pair[1]).ratio()
+        if SIMILAR < similarity < IDENTICAL:
+            yield pair
+
 
 
 if __name__ == "__main__":
